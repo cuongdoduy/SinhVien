c7fabccaafb125be3ceb4cd67ff9dc9e27863d34
cuongdoduy <cuongthtb08022004@gmail.com>
2022-07-18 14:16:05 +0700

    first commit

--- SinhVien.cpp
+++ SinhVien.cpp
@@ -1,7 +1,54 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#include <vector>
 using namespace std;
+class SinhVien
+{
+private:
+    string Id,Name,Class,Email;
+public:
+    SinhVien()
+    {
+        Id=Name=Class=Email="";
+    }
+    friend istream& operator >> (istream&,SinhVien &a);
+    friend ostream& operator << (ostream&,SinhVien a);
+    friend bool operator < (SinhVien a,SinhVien b);
+    string getClass()
+    {
+        return Class;
+    }
+};
+istream& operator >> (istream&,SinhVien &a)
+{
+    cin>>a.Id;
+    cin.ignore();
+    getline(cin,a.Name);
+    cin>>a.Class>>a.Email;
+}
+ostream& operator << (ostream&,SinhVien a)
+{
+    cout<<a.Id<<" "<<a.Name<<" "<<a.Class<<" "<<a.Email<<endl;
+}
 int main()
 {
-    cout<<"hello world";
+    vector<SinhVien> a;
+    map<string,vector<SinhVien>> mp;
+    string s;
+    SinhVien x;
+    int q,n,i;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        cin>>x;
+        mp[x.getClass()].push_back(x);
+    }
+    cin>>q;
+    while(q--)
+    {
+        cin>>s;
+        cout<<"Danh sach Sinh Vien lop "<<s<<": "<<endl;
+        for(SinhVien x:mp[s])
+            cout<<x;
+    }
     return 0;
 }
